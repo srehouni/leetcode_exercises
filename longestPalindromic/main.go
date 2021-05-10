801a9344de901548526f2706c5d628792f9cdd24
Said Rehouni <said_rehouni@hotmail.com>
2021-05-11 00:23:06 +0200

    Added production code to main.go

--- longestPalindromic/main.go
+++ longestPalindromic/main.go
@@ -1,6 +1,44 @@
 package main
 
+import "fmt"
+
 func main() {
-	//array1 := []int{1, 2}
-	//array2 := []int{3, 4}
+	fmt.Println(longestPalindrome("abaxrt"))
+}
+
+func longestPalindrome(s string) string {
+	if len(s) == 1 {
+		return s
+	}
+	var start, end int
+
+	for i := 0; i < len(s); i++ {
+		len1 := palindromeLen(s, i, i)
+		len2 := palindromeLen(s, i, i+1)
+
+		lenMax := max(len1, len2)
+
+		if lenMax > end-start {
+			start = i - ((lenMax - 1) / 2)
+			end = i + lenMax/2
+		}
+	}
+
+	return s[start : end+1]
+}
+
+func palindromeLen(s string, i int, j int) int {
+	for i >= 0 && j < len(s) && s[i] == s[j] {
+		i--
+		j++
+	}
+
+	return j - i - 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
